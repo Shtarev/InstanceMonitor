3e6657509c89188486c83701cd278b149045143c
Andrey Shtarev <54120707+Shtarev@users.noreply.github.com>
2025-10-14 15:08:40 +0200

    Update ExampleClass.php

--- src/ExampleClass.php
+++ src/ExampleClass.php
@@ -2,12 +2,31 @@
 
 namespace Shtarev\PaketExample;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+
 class ExampleClass
 {
-    public $testVar = 'Das ist Test';
+    private $id = '';
+    private $baseUrl = '';
+    private $installationName = '';
+    private $installationType = '';
+    private $installationVersion = '';
+    private $phpVersion = '';
+    private $lastCheck = '';
+    private $sites = '';
+    private $additionalInformations = '';
     
-    public function testFunction()
+    public function testFunction(SerializerInterface $serializer)
     {
-        return $this->testVar;
+        $data = [
+            'name' => 'Test',
+            'version' => '1.0.0',
+        ];
+
+        $json = $serializer->serialize($data, 'json');
+
+        return new Response($json, 200, ['Content-Type' => 'application/json']);
     }
 }
